d862129b46016dffc985beb3ddce513b9d4c8e09
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-08 23:18:01 +0100

    Understanding & Using State (3-38)

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import Person from './Person/Person';
 //Because elements starting with lowercase are native Html elements. 
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name:"Max", age:28 },
+      { name:"Manu", age:29 },
+      { name:"John", age:35 }  
+    ]
+  }
   render() {
     return (
       <div className="App">
           <h1>Hi, I'm React App</h1>
           <p>This is really working!</p>
-          <Person name="Max" age="28" />
-          <Person name="Manu" age="29" />
-          <Person name="John" age="35">Hobbies: Racing</Person>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Hobbies: Racing</Person>
       </div>
     );
   }
