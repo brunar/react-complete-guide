62120d0ffaf6d4ab16c6a4d9b694386a3e876619
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-08 10:38:47 +0100

    Working with Props and Understanding the "children" Prop (3-36,37)
    props.children

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends Component {
       <div className="App">
           <h1>Hi, I'm React App</h1>
           <p>This is really working!</p>
-          <Person />
-          <Person />
-          <Person></Person>
+          <Person name="Max" age="28" />
+          <Person name="Manu" age="29" />
+          <Person name="John" age="35">Hobbies: Racing</Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
+//There is a special props.children 
+//It will access the property inside the tags wrapped including plain text
 
-const person = () => {
- return <p>I'm a Person! and I'm {Math.floor(Math.random() * 30)} years old!</p>
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm a {props.name} and I'm {props.age} years old!</p>
+            <p>{props.children}</p> 
+        </div>
+    )
 }
 
 export default person;
