3a34b338abdd1dba98bd17d77154feaebec86b69
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-08 10:30:19 +0100

    Creating a Functional Component (3-32)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'; 
+//The name of the component should be Uppercase (for components es6)
+//Because elements starting with lowercase are native Html elements. 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
-          <h1>Bruna</h1>
+          <h1>Hi, I'm React App</h1>
+          <p>This is really working!</p>
+          <Person />
+          <Person />
+          <Person></Person>
       </div>
     );
   }
