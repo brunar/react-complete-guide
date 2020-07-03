64acd0c12c1967bd54618ac3c8a3bdd7f4d6bdf6
Bruna Radeke <brunaradeke@hotmail.com>
2020-07-03 01:16:44 +0100

    Adding BrowserRouter basename path to brunajs and centered all body

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App appTitle="Person Manager" />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter basename="/view/react-complete-guide">
+        <App appTitle="Person Manager" />
+    </BrowserRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
